7a60e72592d3ca3496ea564c3aceaa34026e757a
hasna <manelhasnahaddoud@gmail.com>
2023-09-24 16:52:22 +0100

    done

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,11 +1,26 @@
 import React from "react";
+import {useState} from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories,onCategoryChange }) {
+  const [selectedCata,setSelectedCata] = useState("All");
+
+  function handleCate (category){
+    setSelectedCata(category);
+    onCategoryChange(category);
+  }
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
-    </div>
+      {categories.map((category) => (
+      <button
+        key={category}
+        className={selectedCata === category ? "selected" : ""}
+        onClick={() => handleCate(category)}
+      >
+        {category}
+      </button>
+    ))}
+  </div>
   );
 }
 
